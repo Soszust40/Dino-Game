23e7a9eaf768fa1ea8222972278b8a63e25e74f2
Soszust40 <soszust40@gmail.com>
2025-10-09 17:28:48 -0400

    MacOS Support

--- build_mac.sh
+++ build_mac.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-python3 -m PyInstaller Bin/Dino_Game.py \
-    --noconsole \
-    --noconfirm \
-    --icon="Data/exeIcon.ico" \
-    --add-data "Data:Data" \
-    --add-data "Bin:Bin"
 
+python3 -m PyInstaller Bin/Dino_Game.py \
+    --name "Dino Game" \
+    --noconfirm \
+    --windowed \
+    --icon="Data/exeIcon.icns" \
+    --hidden-import=PySide6 \
+    --hidden-import=PySide6.QtCore \
+    --hidden-import=PySide6.QtGui \
+    --hidden-import=PySide6.QtWidgets \
+    --add-data "Data:Data"
