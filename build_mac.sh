aa53c78fe2c18761704e3002be605c7b6fb5c70e
Soszust40 <soszust40@gmail.com>
2025-10-09 12:45:49 -0400

    Added Icon

--- build_mac.sh
+++ build_mac.sh
@@ -2,6 +2,7 @@
 python3 -m PyInstaller Bin/Dino_Game.py \
     --noconsole \
     --noconfirm \
+    --icon="Data/exeIcon.ico" \
     --add-data "Data:Data" \
     --add-data "Bin:Bin"
 
--- build_win.bat
+++ build_win.bat
@@ -2,6 +2,7 @@
 python -m PyInstaller ^
     --noconsole ^
     --noconfirm ^
+    --icon="Data/exeIcon.ico" ^
     --add-data "Data;Data" ^
     --add-data "Bin;Bin" ^
     Bin/Dino_Game.py
