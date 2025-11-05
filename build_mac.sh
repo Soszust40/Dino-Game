ff1cc4d221b3a40b23dc028d6ca929e9d6483b39
Soszust40 <soszust40@gmail.com>
2025-11-04 19:23:47 -0500

    Update build_mac.sh

--- build_mac.sh
+++ build_mac.sh
@@ -4,11 +4,11 @@ python3 -m PyInstaller Bin/Dino_Game.py \
     --name "Dino Game" \
     --noconfirm \
     --windowed \
-    --onefile \
     --icon="Data/UI/exeIcon.icns" \
     --hidden-import=PySide6 \
     --hidden-import=PySide6.QtCore \
     --hidden-import=PySide6.QtGui \
     --hidden-import=PySide6.QtWidgets \
+    --hidden-import=requests \
     --add-data "Data:Data" \
     --add-data "translations:translations"
